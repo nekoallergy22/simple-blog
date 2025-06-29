3cb6b6dc2564c52f3962497369f66db4f181092b
nekoallergy22 <takaliate@gmail.com>
2025-06-29 17:00:17 +0900

    Firebase Functions → Cloud Run 移行完了

--- api/dist/handlers.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response } from 'express';
-export declare const syncMarkdownHandler: (req: Request, res: Response) => Promise<void>;
-export declare const healthCheckHandler: (req: Request, res: Response) => void;
-//# sourceMappingURL=handlers.d.ts.map
